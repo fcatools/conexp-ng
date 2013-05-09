cf11ac7fcff8165309ff0ba4e6940f2f702aae1f
David <david.22.7@gmx.de>
2013-05-09 12:19:50 +0200

    added a little tabbedPlane

--- src/main/java/com/eugenkiss/conexp2/Main.java
+++ src/main/java/com/eugenkiss/conexp2/Main.java
@@ -1,21 +1,55 @@
 package com.eugenkiss.conexp2;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class Main extends JFrame {
-	
+
 	private static final long serialVersionUID = -3768163989667340886L;
 
 	public static void main(String... args) {
 		new Main();
 	}
-	
+
 	Main() {
-		JLabel lbl = new JLabel("Hello World");
-		add(lbl);
-		this.setSize(100, 100);
+		
+		// litte TabbedPane-test
+		final JTabbedPane pane = new JTabbedPane();
+		for (int i = 0; i < 4; i++) {
+			String title = "Tab " + i;
+			pane.insertTab(title, null, new TabComponent(i), null, i);
+		}
+		pane.setTabPlacement(JTabbedPane.BOTTOM);
+
+		add(pane);
+		setLocationRelativeTo(null);
+		setSize(400, 200);
 		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	private class TabComponent extends JPanel {
+
+		private static final long serialVersionUID = 8653733413060081454L;
+
+		public TabComponent(final int i) {
+			JLabel label = new JLabel() {
+
+				private static final long serialVersionUID = -9116966818508859332L;
+
+				public String getText() {
+
+					return "Tab " + i;
+				}
+			};
+			label.setHorizontalAlignment(JLabel.CENTER);
+			setLayout(new GridLayout(1, 1));
+			add(label);
+		}
+
+	}
 }
