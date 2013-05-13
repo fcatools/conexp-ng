cfb8338ebcb3ae3cc58f869f0c71b8696a338aba
Eugen Kiss <eugen@eugenkiss.com>
2013-05-13 14:27:24 +0200

    Remove superfluent import

--- src/main/java/com/eugenkiss/conexp2/Main.java
+++ src/main/java/com/eugenkiss/conexp2/Main.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
