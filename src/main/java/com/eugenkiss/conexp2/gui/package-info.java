fd52e5f4da24f84e7a98653937e30c66cdc11a5a
Eugen Kiss <eugen@eugenkiss.com>
2013-05-22 21:54:53 +0200

    Add documentation file to gui package & 'mvn javadoc:javadoc' command
    We should get accustomed to provide comments & documentation in the
    code. FYI, it is possible to add package documentation by creating
    a 'package-info.java' file under the package like I did in this
    commit. To create JavaDocs simply run 'mvn javadoc:javadoc' in the
    working directory. The generated HTML files can then be found under
    'target/site/apidocs'.

---
+++ src/main/java/com/eugenkiss/conexp2/gui/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * This package contains all GUI-related classes.
+ * <p>
+ * Each class roughly corresponds to a visible GUI component.
+ * <p>
+ * The code adheres to the following style.
+ * <ul>
+ * <li>{@link javax.swing.Action Action}s and 
+ *     {@link javax.swing.ActionMap ActionMap}s are preferred over 
+ *     {@link javax.swing.ActionListener ActionListener}s.
+ * <li>{@link javax.swing.event.ChangeListener ChangeListener}s and 
+ *     {@link java.beans.PropertyChangeListener PropertyChangeListener}s are preferred over 
+ *     {@link java.util.Observer Observer}s and
+ *     {@link java.util.Observable Observable}s.
+ * </ul>
+ * In general, the recommendations from the following links are followed:
+ * <ul>
+ * <li><a href="http://stackoverflow.com/questions/1742001/java-awt-swt-swing-how-to-plan-a-gui/1742104#1742104">Java AWT/SWT/Swing: How to plan a GUI?</a>
+ * <li><a href="http://stackoverflow.com/questions/5473828/java-swing-gui-best-practices-from-a-code-standpoint">Java/Swing GUI best practices</a>
+ * </ul>
+ */
+package com.eugenkiss.conexp2.gui;
