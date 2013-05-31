5665f7e370f5e1c193a4b9bd6f6ad8db4d4acbeb
David <david.22.7@gmx.de>
2013-05-31 18:52:40 +0200

    implemented getstembase() in our FormalContext and a suportCount-method, you can see the implications of the actual context in the implication-view now

--- src/main/java/com/eugenkiss/conexp2/gui/ImplicationView.java
+++ src/main/java/com/eugenkiss/conexp2/gui/ImplicationView.java
@@ -1,19 +1,95 @@
 package com.eugenkiss.conexp2.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.Set;
+
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 
 import com.eugenkiss.conexp2.ProgramState;
+import de.tudresden.inf.tcs.fcaapi.FCAImplication;
 
 public class ImplicationView extends View {
 
     private static final long serialVersionUID = -6377834669097012170L;
 
+    private JTextPane area = new JTextPane();
+
+    private Set<FCAImplication<String>> implications;
+
+    private SimpleAttributeSet[] attrs;
+    final int NON_ZERO_SUPPORT_STYLE = 0;
+    final int ZERO_SUPPORT_STYLE = 1;
+
+    protected final String FOLLOW = " ==> ";
+
+    protected final String END_MARK = ";";
+
+    private final String EOL = System.getProperty("line.separator");
+
     public ImplicationView(ProgramState state) {
         super(state);
-        view = new JScrollPane(new JTextArea());
-        settings = new AssociationSettings();
-        super.init();
+
+        attrs = new SimpleAttributeSet[2];
+        attrs[NON_ZERO_SUPPORT_STYLE] = new SimpleAttributeSet();
+        StyleConstants.setForeground(attrs[NON_ZERO_SUPPORT_STYLE], Color.blue);
+
+        attrs[ZERO_SUPPORT_STYLE] = new SimpleAttributeSet();
+        StyleConstants.setForeground(attrs[ZERO_SUPPORT_STYLE], Color.red);
+
+        view = new JScrollPane(area);
+        setLayout(new BorderLayout());
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        toolbar.setFloatable(false);
+
+        panel.add(toolbar, BorderLayout.WEST);
+        panel.add(view, BorderLayout.CENTER);
+        add(panel);
+
+
+        updateImplications();
+        writeImplications();
+    }
+
+    private void writeImplications() {
+        int i = 1, support = 0;
+        StringBuffer buf;
+        System.out.println(implications);
+        for (FCAImplication<String> impl : implications) {
+            support = state.context.supportCount(impl.getPremise());
+            buf = new StringBuffer();
+            buf.append(i);
+            buf.append("< " + support + " > ");
+            buf.append(impl.getPremise() + FOLLOW + impl.getConclusion()
+                    + END_MARK);
+            buf.append(EOL);
+            i++;
+            try {
+                area.getDocument().insertString(area.getDocument().getLength(),
+                        buf.toString(), implicationStyle(support));
+            } catch (BadLocationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public javax.swing.text.SimpleAttributeSet implicationStyle(int support) {
+        return 0 == support ? attrs[ZERO_SUPPORT_STYLE]
+                : attrs[NON_ZERO_SUPPORT_STYLE];
+    }
+
+    private void updateImplications() {
+        implications = state.context.getStemBase();
     }
 
 }
--- src/main/java/com/eugenkiss/conexp2/model/FormalContext.java
+++ src/main/java/com/eugenkiss/conexp2/model/FormalContext.java
@@ -1,37 +1,111 @@
 package com.eugenkiss.conexp2.model;
 
+import java.util.Set;
+
+import de.tudresden.inf.tcs.fcaapi.FCAImplication;
 import de.tudresden.inf.tcs.fcaapi.utils.IndexedSet;
 import de.tudresden.inf.tcs.fcalib.FullObject;
+import de.tudresden.inf.tcs.fcalib.Implication;
+import de.tudresden.inf.tcs.fcalib.ImplicationSet;
 import de.tudresden.inf.tcs.fcalib.utils.ListSet;
 
 /**
- * A specialization of FormalContext<String,String> with the aim to remove
- * the verbose repetition of <String,String>. Plus, adds a couple of useful
- * methods.
+ * A specialization of FormalContext<String,String> with the aim to remove the
+ * verbose repetition of <String,String>. Plus, adds a couple of useful methods.
  */
-public class FormalContext extends de.tudresden.inf.tcs.fcalib.FormalContext<String, String> {
+public class FormalContext extends
+        de.tudresden.inf.tcs.fcalib.FormalContext<String, String> {
 
     public FormalContext() {
         super();
     }
 
+    public int supportCount(Set<String> attributes) {
+        int count = 0;
+        boolean notfound;
+        for (FullObject<String, String> obj : objects) {
+            notfound = false;
+            for (String att : attributes) {
+                if (!objectHasAttribute(obj, att)) {
+                    notfound = true;
+                    break;
+                }
+            }
+            if (!notfound)
+                count++;
+        }
+        return count;
+
+    }
+
+    @Override
+    public Set<FCAImplication<String>> getStemBase() {
+        // de.tudresden.inf.tcs.fcalib.ImplicationSet<String> doesn't return the
+        // implications, so we need this result-variable, maybe we should modify
+        // ImplicationSet
+        IndexedSet<FCAImplication<String>> result = new ListSet<>();
+
+        ImplicationSet<String> impl = new ImplicationSet<>(this);
+
+        // Next-Closure fca.04 implications page 14
+
+        Set<String> A = new ListSet<>();
+
+        while (!A.equals(getAttributes())) {
+            A = impl.nextClosure(A);
+            if (!A.equals(doublePrime(A))) {
+                Implication<String> im = new Implication<>(A, doublePrime(A));
+                impl.add(im);
+                result.add(im);
+            }
+        }
+
+        return andNot(result);
+    }
+
+    /**
+     * Removes the elements of the conclusion which are also in the premise for
+     * all given implications
+     *
+     * @param implications
+     * @return
+     */
+    private Set<FCAImplication<String>> andNot(
+            Set<FCAImplication<String>> implications) {
+        Set<FCAImplication<String>> result = new ListSet<>();
+        Set<String> conclusion;
+        for (FCAImplication<String> fcaImplication : implications) {
+            conclusion = fcaImplication.getConclusion();
+            for (String premise : fcaImplication.getPremise()) {
+                if (fcaImplication.getConclusion().contains(premise)) {
+                    conclusion.remove(premise);
+                }
+            }
+            result.add(new Implication<>(fcaImplication.getPremise(),
+                    conclusion));
+        }
+        return result;
+    }
+
     public void transpose() {
-        IndexedSet<FullObject<String,String>> newObjects = new ListSet<>();
+        IndexedSet<FullObject<String, String>> newObjects = new ListSet<>();
         IndexedSet<String> newAttributes = new ListSet<>();
         for (String attribute : getAttributes()) {
             IndexedSet<String> allObjectsForAttribute = new ListSet<>();
-            // TODO: there may be a more efficient way using the fcalib functions
-            for (FullObject<String,String> object : objects) {
+            // TODO: there may be a more efficient way using the fcalib
+            // functions
+            for (FullObject<String, String> object : objects) {
                 if (objectHasAttribute(object, attribute))
                     allObjectsForAttribute.add(object.getIdentifier());
             }
             newObjects.add(new FullObject<>(attribute, allObjectsForAttribute));
         }
-        for (FullObject<String,String> object : objects) {
+        for (FullObject<String, String> object : objects) {
             newAttributes.add(object.getIdentifier());
         }
         objects = newObjects;
-        // Why can I access objects directly but not attributes? (I'm questioning the API-decision)
+        // Why can I access objects directly but not attributes? (I'm
+        // questioning the API-decision)
         getAttributes().clear();
         for (String attribute : newAttributes) {
             getAttributes().add(attribute);
