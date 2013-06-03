f99caceb17037f5d6a19b1de6feb8117d5fc6883
Eugen Kiss <eugen@eugenkiss.com>
2013-06-03 16:38:22 +0200

    Activate anti-aliasing for lattice

--- src/main/java/com/eugenkiss/conexp2/gui/LatticeGraphView.java
+++ src/main/java/com/eugenkiss/conexp2/gui/LatticeGraphView.java
@@ -1,14 +1,12 @@
 package com.eugenkiss.conexp2.gui;
 
-import java.awt.Color;
-import java.awt.Graphics;
-
-import org.apache.batik.swing.JSVGCanvas;
-
 import com.eugenkiss.conexp2.draw.Edge;
 import com.eugenkiss.conexp2.draw.LatticeGraph;
 import com.eugenkiss.conexp2.draw.LatticeGraphNodeMouseMotionListener;
 import com.eugenkiss.conexp2.draw.Node;
+import org.apache.batik.swing.JSVGCanvas;
+
+import java.awt.*;
 
 /*The JSVGCanvas provides a set of build-in interactors that
  * let the users manipulate the displayed document, including ones for zooming,
@@ -30,8 +28,11 @@ public class LatticeGraphView extends JSVGCanvas {
     }
 
     @Override
-    public void paint(Graphics g) {
+    public void paint(Graphics g0) {
+        Graphics2D g = (Graphics2D) g0;
         super.paint(g);
+        g.addRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, getWidth(), getHeight());
        // g.drawOval(300, 300, 50, 50);
