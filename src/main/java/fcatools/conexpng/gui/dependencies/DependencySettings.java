9ec698551e9930c65325181277fd6ed04dc6b89e
David <david.22.7@gmx.de>
2013-07-20 20:33:42 +0200

    the calculation of the associations is canceled now if you change the slider

--- src/main/java/fcatools/conexpng/gui/dependencies/DependencySettings.java
+++ src/main/java/fcatools/conexpng/gui/dependencies/DependencySettings.java
@@ -18,222 +18,227 @@ import java.beans.PropertyChangeSupport;
 
 public class DependencySettings extends JPanel {
 
-	private PropertyChangeSupport propertyChangeSupport;
+    private PropertyChangeSupport propertyChangeSupport;
 
-	private static final long serialVersionUID = -3692280021161777005L;
+    private static final long serialVersionUID = -3692280021161777005L;
 
-	WebLabel minSupLabel = new WebLabel("Support");
-	WebTextField supField = new WebTextField("0.1");
-	WebSlider minSupSlider = new WebSlider(0, 100, 10);
+    WebLabel minSupLabel = new WebLabel("Support");
+    WebTextField supField = new WebTextField("0.1");
+    WebSlider minSupSlider = new WebSlider(0, 100, 10);
 
-	WebLabel confLabel = new WebLabel("Confidence");
-	WebTextField confField = new WebTextField("0.5");
-	WebSlider confSlider = new WebSlider(0, 100, 50);
+    WebLabel confLabel = new WebLabel("Confidence");
+    WebTextField confField = new WebTextField("0.5");
+    WebSlider confSlider = new WebSlider(0, 100, 50);
 
-	// Only for testing
-	private int current = 0, all = 0;
+    // Only for testing
+    private int current = 0, all = 0;
 
-	@SuppressWarnings("serial")
-	WebPanel piechart = new WebPanel() {
+    @SuppressWarnings("serial")
+    WebPanel piechart = new WebPanel() {
 
-		@Override
-		public void paint(Graphics g2) {
+        @Override
+        public void paint(Graphics g2) {
 
-			super.paint(g2);
-			Graphics2D g = (Graphics2D) g2;
-			g.addRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
-			g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            super.paint(g2);
+            Graphics2D g = (Graphics2D) g2;
+            g.addRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
+            g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
-			int degree = (int) ((current * 360.0) / all);
-			g.setColor(getBackground());
-			g.fillRect(0, 0, getWidth(), getHeight());
-			Color gray = new Color(225, 225, 225);
-			g.setPaint(gray);
-			g.fillArc(0, 5, 140, 140, 0, 360);
-			g.setPaint(gray.darker().darker());
-			g.drawString("#Association rules = " + all, 0, 165);
+            int degree = (int) ((current * 360.0) / all);
+            g.setColor(getBackground());
+            g.fillRect(0, 0, getWidth(), getHeight());
+            Color gray = new Color(225, 225, 225);
+            g.setPaint(gray);
+            g.fillArc(0, 5, 140, 140, 0, 360);
+            g.setPaint(gray.darker().darker());
+            g.drawString("#Association rules = " + all, 0, 165);
 
-			Color green = new Color(150, 220, 150);
-			g.setColor(green);
-			g.fillArc(0, 5, 140, 140, 90, degree);
-			g.setColor(green.darker().darker());
-			g.drawString("#With support = " + current, 0, 180);
+            Color green = new Color(150, 220, 150);
+            g.setColor(green);
+            g.fillArc(0, 5, 140, 140, 90, degree);
+            g.setColor(green.darker().darker());
+            g.drawString("#With support = " + current, 0, 180);
 
-			// g.setColor(Color.BLACK);
-			// Shape circ = new Ellipse2D.Double(0, 5, 140, 140);
-			// g.draw(circ);
+            // g.setColor(Color.BLACK);
+            // Shape circ = new Ellipse2D.Double(0, 5, 140, 140);
+            // g.draw(circ);
 
-		}
+        }
 
-	};
+    };
 
-	public DependencySettings() {
-		propertyChangeSupport = new PropertyChangeSupport(this);
-		setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		minSupSlider.setPreferredSize(new Dimension(150, 25));
-		confSlider.setPreferredSize(new Dimension(150, 25));
-		gbc.anchor = GridBagConstraints.WEST;
-		gbc.insets = new Insets(10, 5, 0, 0);
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		add(minSupLabel, gbc);
-		gbc.gridx = 1;
-		supField.setPreferredSize(new Dimension(40, 20));
-		add(supField, gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		gbc.gridwidth = 2;
-		add(minSupSlider, gbc);
-		gbc.gridwidth = 1;
-		gbc.gridy = 2;
-		add(confLabel, gbc);
-		gbc.gridx = 1;
-		confField.setPreferredSize(new Dimension(40, 20));
-		add(confField, gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		gbc.gridwidth = 2;
-		add(confSlider, gbc);
-		gbc.gridy = 4;
-		piechart.setPreferredSize(new Dimension(150, 200));
-		add(piechart, gbc);
-		gbc.gridy = 5;
-		add(new WebLabel("Sorting by:"), gbc);
-		Action sortAction = new SortAction();
-		WebRadioButton lexical = new WebRadioButton();
-		lexical.setSelected(true);
-		lexical.setAction(sortAction);
-		lexical.setText("Lexical order");
-		lexical.setMnemonic(KeyEvent.VK_L);
-		lexical.setActionCommand("LexicalOrder");
+    public DependencySettings() {
+        propertyChangeSupport = new PropertyChangeSupport(this);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        minSupSlider.setPreferredSize(new Dimension(150, 25));
+        confSlider.setPreferredSize(new Dimension(150, 25));
+        gbc.insets = new Insets(10, 5, 0, 0);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        WebLabel title = new WebLabel("Associations Settings");
+        add(title, gbc);
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        add(minSupLabel, gbc);
+        gbc.gridx = 1;
+        supField.setPreferredSize(new Dimension(40, 20));
+        add(supField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        add(minSupSlider, gbc);
+        gbc.gridwidth = 1;
+        gbc.gridy = 3;
+        add(confLabel, gbc);
+        gbc.gridx = 1;
+        confField.setPreferredSize(new Dimension(40, 20));
+        add(confField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 2;
+        add(confSlider, gbc);
+        gbc.gridy = 5;
+        piechart.setPreferredSize(new Dimension(150, 200));
+        add(piechart, gbc);
+        gbc.gridy = 6;
+        add(new WebLabel("Sorting by:"), gbc);
+        Action sortAction = new SortAction();
+        WebRadioButton lexical = new WebRadioButton();
+        lexical.setSelected(true);
+        lexical.setAction(sortAction);
+        lexical.setText("Lexical order");
+        lexical.setMnemonic(KeyEvent.VK_L);
+        lexical.setActionCommand("LexicalOrder");
 
-		WebRadioButton support = new WebRadioButton();
-		support.setAction(sortAction);
-		support.setText("Support");
-		support.setMnemonic(KeyEvent.VK_S);
-		support.setActionCommand("Support");
+        WebRadioButton support = new WebRadioButton();
+        support.setAction(sortAction);
+        support.setText("Support");
+        support.setMnemonic(KeyEvent.VK_S);
+        support.setActionCommand("Support");
 
-		// Group the radio buttons.
-		ButtonGroup group = new ButtonGroup();
-		group.add(lexical);
-		group.add(support);
-		gbc.gridy = 6;
-		add(lexical, gbc);
-		gbc.gridy = 7;
-		add(support, gbc);
+        // Group the radio buttons.
+        ButtonGroup group = new ButtonGroup();
+        group.add(lexical);
+        group.add(support);
+        gbc.gridy = 7;
+        add(lexical, gbc);
+        gbc.gridy = 8;
+        add(support, gbc);
 
-		confSlider.addChangeListener(new SliderListener(false));
-		minSupSlider.addChangeListener(new SliderListener(true));
-		confField.addKeyListener(new TextFieldAction(false));
-		supField.addKeyListener(new TextFieldAction(true));
-	}
+        confSlider.addChangeListener(new SliderListener(false));
+        minSupSlider.addChangeListener(new SliderListener(true));
+        confField.addKeyListener(new TextFieldAction(false));
+        supField.addKeyListener(new TextFieldAction(true));
+    }
 
-	public void update(int numberOfCurrentAssocitaionrules, int numberOfAllCurrentAssocitaionrules) {
-		current = numberOfCurrentAssocitaionrules;
-		all = numberOfAllCurrentAssocitaionrules;
-		piechart.repaint();
-	}
+    public void update(int numberOfCurrentAssocitaionrules, int numberOfAllCurrentAssocitaionrules) {
+        current = numberOfCurrentAssocitaionrules;
+        all = numberOfAllCurrentAssocitaionrules;
+        piechart.repaint();
+    }
 
-	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		super.addPropertyChangeListener(listener);
-		propertyChangeSupport.addPropertyChangeListener(listener);
-	}
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        super.addPropertyChangeListener(listener);
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
 
-	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		super.removePropertyChangeListener(listener);
-		propertyChangeSupport.removePropertyChangeListener(listener);
-	}
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        super.removePropertyChangeListener(listener);
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
 
-	private void myFirePropertyChange(String propertyName, Object oldValue, Object newValue) {
-		propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
-	}
+    private void myFirePropertyChange(String propertyName, Object oldValue, Object newValue) {
+        propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
 
-	// ////////////////////////////////////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////////////////////////////////////
 
-	@SuppressWarnings("serial")
-	private class SortAction extends AbstractAction {
+    @SuppressWarnings("serial")
+    private class SortAction extends AbstractAction {
 
-		boolean lex = true;
+        boolean lex = true;
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			if (arg0.getActionCommand().equals("LexicalOrder")) {
-				if (lex)
-					return;
-				else {
-					lex = true;
-					myFirePropertyChange("ToggleSortingOrder", null, null);
-				}
-			} else {
-				if (!lex)
-					return;
-				else {
-					lex = false;
-					myFirePropertyChange("ToggleSortingOrder", null, null);
-				}
-			}
-		}
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            if (arg0.getActionCommand().equals("LexicalOrder")) {
+                if (lex)
+                    return;
+                else {
+                    lex = true;
+                    myFirePropertyChange("ToggleSortingOrder", null, null);
+                }
+            } else {
+                if (!lex)
+                    return;
+                else {
+                    lex = false;
+                    myFirePropertyChange("ToggleSortingOrder", null, null);
+                }
+            }
+        }
 
-	}
+    }
 
-	private class TextFieldAction implements KeyListener {
+    private class TextFieldAction implements KeyListener {
 
-		private boolean minSup;
+        private boolean minSup;
 
-		public TextFieldAction(boolean minSup) {
-			this.minSup = minSup;
-		}
+        public TextFieldAction(boolean minSup) {
+            this.minSup = minSup;
+        }
 
-		@Override
-		public void keyPressed(KeyEvent e) {
-			// nothing todo
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // nothing todo
 
-		}
+        }
 
-		@Override
-		public void keyReleased(KeyEvent e) {
-			// nothing todo
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // nothing todo
 
-		}
+        }
 
-		@Override
-		public void keyTyped(KeyEvent e) {
+        @Override
+        public void keyTyped(KeyEvent e) {
 
-			if (e.getKeyChar() == KeyEvent.VK_ENTER) {
-				if (minSup) {
-					double value = Double.parseDouble(supField.getText());
-					minSupSlider.setValue((int) (value * 100));
-				} else {
-					double value = Double.parseDouble(confField.getText());
-					confSlider.setValue((int) (value * 100));
-				}
-			} else if (!(e.getKeyChar() == '.' && ((WebTextField) e.getSource()).getText().indexOf('.') < 0)
-					&& !(e.getKeyChar() >= '0' && e.getKeyChar() <= '9')) {
-				e.consume();
-			}
+            if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+                if (minSup) {
+                    double value = Double.parseDouble(supField.getText());
+                    minSupSlider.setValue((int) (value * 100));
+                } else {
+                    double value = Double.parseDouble(confField.getText());
+                    confSlider.setValue((int) (value * 100));
+                }
+            } else if (!(e.getKeyChar() == '.' && ((WebTextField) e.getSource()).getText().indexOf('.') < 0)
+                    && !(e.getKeyChar() >= '0' && e.getKeyChar() <= '9')) {
+                e.consume();
+            }
 
-		}
-	}
+        }
+    }
 
-	private class SliderListener implements ChangeListener {
+    private class SliderListener implements ChangeListener {
 
-		private boolean minSup;
+        private boolean minSup;
 
-		public SliderListener(boolean minSup) {
-			this.minSup = minSup;
-		}
+        public SliderListener(boolean minSup) {
+            this.minSup = minSup;
+        }
 
-		public void stateChanged(ChangeEvent e) {
-			WebSlider slider = (WebSlider) e.getSource();
-			double value = slider.getValue() / 100.0;
-			if (minSup) {
-				supField.setText("" + value);
-				DependencySettings.this.myFirePropertyChange("MinimalSupportChanged", 0, value);
-			} else {
-				confField.setText("" + value);
-				DependencySettings.this.myFirePropertyChange("ConfidenceChanged", 0, value);
-			}
-		}
-	}
+        public void stateChanged(ChangeEvent e) {
+            WebSlider slider = (WebSlider) e.getSource();
+            double value = slider.getValue() / 100.0;
+            if (minSup) {
+                supField.setText("" + value);
+                if (!slider.getValueIsAdjusting()) {
+                    DependencySettings.this.myFirePropertyChange("MinimalSupportChanged", 0, value);
+                }
+            } else {
+                confField.setText("" + value);
+                DependencySettings.this.myFirePropertyChange("ConfidenceChanged", 0, value);
+            }
+        }
+    }
 }
