0da4af982710d99046e240a01b6a8c39b6e114b1
David <david.22.7@gmx.de>
2013-07-24 21:10:29 +0200

    completes last commit havn't seen, that it was not selected :)

---
+++ src/main/java/fcatools/conexpng/gui/MainStatusBar.java
@@ -0,0 +1,93 @@
+package fcatools.conexpng.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.SwingWorker;
+
+import com.alee.laf.label.WebLabel;
+import com.alee.laf.panel.WebPanel;
+import com.alee.laf.progressbar.WebProgressBar;
+import fcatools.conexpng.ProgramState;
+import fcatools.conexpng.ProgramState.StatusBarMessage;
+
+@SuppressWarnings("serial")
+public class MainStatusBar extends WebPanel implements PropertyChangeListener {
+
+    public MainStatusBar() {
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(100, 20));
+    }
+
+    private class StatusBar extends SwingWorker<Void, Void> {
+
+        private WebLabel label = null;
+
+        @Override
+        protected Void doInBackground() throws Exception {
+            WebPanel panel = new WebPanel(new BorderLayout());
+            label = new WebLabel("");
+            panel.add(label, BorderLayout.CENTER);
+            WebProgressBar progressbar = new WebProgressBar();
+            progressbar.setIndeterminate(true);
+            panel.add(progressbar, BorderLayout.EAST);
+            removeAll();
+            MainStatusBar.this.add(panel, BorderLayout.EAST);
+            revalidate();
+            getParent().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+            while (!isCancelled()) {
+                label.setText(text);
+            }
+            return null;
+        }
+
+        @Override
+        protected void done() {
+            super.done();
+            getParent().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+            MainStatusBar.this.removeAll();
+            MainStatusBar.this.add(new WebLabel(""));
+            revalidate();
+
+        }
+
+    }
+
+    private StatusBar bar;
+    private String text = "";
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt instanceof StatusBarMessage) {
+            StatusBarMessage status = (StatusBarMessage) evt;
+            if ((int) status.getNewValue() == ProgramState.START) {
+                if (bar == null || bar.isDone()) {
+                    bar = new StatusBar();
+                    bar.execute();
+                }
+                if (text.isEmpty()) {
+                    text = status.getPropertyName();
+                } else {
+                    if (!text.contains(status.toString()))
+                        ;
+                    text += ", " + status.getPropertyName();
+                }
+            } else if ((int) status.getNewValue() == ProgramState.STOP) {
+
+                text = text.replace(status.getPropertyName(), "");
+                text = text.replace(", , ", ", ");
+                if (text.endsWith(", "))
+                    text = text.substring(0, text.lastIndexOf(','));
+                if (text.startsWith(", "))
+                    text = text.substring(2);
+                if (text.trim().isEmpty() && bar != null) {
+                    bar.cancel(true);
+                }
+            }
+
+        }
+    }
+}
